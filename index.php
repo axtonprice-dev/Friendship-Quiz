6a5f194046f8204d0bd4f143d348ca6464c492c9
Axton <priceroan2@gmail.com>
2022-05-25 01:46:37 +0100

    🐛 Functions Commit

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
+error_reporting(0); // Disable errors
+
+/* Application Configuration */
+$config = array(
+    "site_name" => "Friend Quiz",
+    "site_name_long" => "The True Friend Quiz",
+    "site_desc" => "How well do your friends truly know you?",
+    "site_title" => "Create a quiz to test how well your friends know you!",
+);
+$variables = array(
+    "date_year" => date("Y"),
+    "date_month" => date("M"),
+    "client_ip" => $_SERVER["HTTP_CF_CONNECTING_IP"] || $_SERVER["REMOTE_ADDR"],
+);
+
 /* Initialise Application */
-require("Modules/main.php"); 
+require("modules/main.php");
+
 ?>
